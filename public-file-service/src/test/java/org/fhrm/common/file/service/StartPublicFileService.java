7ad247a1fbd3d51c9d95a240ddf9e9d795df0052
admin <admin@DESKTOP-5PBCT3P>
2018-12-19 18:42:35 +0800

    11111111111

---
+++ public-file-service/src/test/java/org/fhrm/common/file/service/StartPublicFileService.java
@@ -0,0 +1,9 @@
+package org.fhrm.common.file.service;
+
+import com.yaozhong.framework.base.start.StartDubboService;
+
+public class StartPublicFileService {
+    public static void main(String[] args) throws Exception {
+        StartDubboService.main(args);
+    }
+}
